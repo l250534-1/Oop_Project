ffcc4e29b67f1200bf218eac0e039a04bd1d81da
l250534-1 <l250534@lhr.nu.edu.pk>
2026-05-03 19:42:08 +0500

    Fix AcademicEntity files placed in correct src and include folders

---
+++ oop_project/src/academicentity.cpp
@@ -0,0 +1,21 @@
+#include "AcademicEntity.h"
+#include <iostream>
+using namespace std;
+
+AcademicEntity::AcademicEntity(string id, string name, string email) {
+    this->id = id;
+    this->name = name;
+    this->email = email;
+}
+
+string AcademicEntity::getId() {
+    return id;
+}
+string AcademicEntity::getName() {
+    return name;
+}
+string AcademicEntity::getEmail() { 
+    return email;
+}
+
+AcademicEntity::~AcademicEntity() {}
