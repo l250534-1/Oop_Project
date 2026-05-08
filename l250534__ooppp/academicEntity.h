7d2ddc40ba4ad54bcbe6d62985a031cf33e1a60f
l250534-1 <l250534@lhr.nu.edu.pk>
2026-05-08 09:44:08 +0500

    saved files

--- l250534__ooppp/academicEntity.h
+++ l250534__ooppp/academicEntity.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef ACADEMICENTITY_H
+#define ACADEMICENTITY_H
+
+#include <string>
+using namespace std;
+
+class AcademicEntity {
+
+protected:
+    string id;
+    string name;
+    string email;
+
+public:
+    AcademicEntity(string id, string name, string email);
+    virtual void displayProfile() = 0;
+    string getId();
+    string getName();
+    string getEmail();
+    virtual ~AcademicEntity();
+};
+
+#endif
