1b3c7952fcb61e550163f4d7fc61ae37c380836f
l250534-1 <l250534@lhr.nu.edu.pk>
2026-05-09 19:52:47 +0500

    Datababase manager
    for student and teacher(cpp) header file for all

---
+++ l250534__ooppp/DatabaseManager.cpp
@@ -0,0 +1,119 @@
+#include "DatabaseManager.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+DatabaseManager::DatabaseManager()
+{
+    studentsFile = "Students.txt";
+    teachersFile = "Teachers.txt";
+    venuesFile = "Venues.txt";
+    sectionsFile = "Sections.txt";
+}
+
+string DatabaseManager::getPart(string line, int index)
+{
+    int    count = 0;
+    string part = "";
+    for (int i = 0; i < (int)line.length(); i++)
+    {
+        if (line[i] == '|')
+        {
+            if (count == index) 
+                return part;
+            count++;
+            part = "";
+        }
+        else
+        {
+            part += line[i];
+        }
+    }
+    if (count == index) 
+        return part;
+    return "";
+}
+
+void DatabaseManager::saveTeacher(Teacher& t) 
+{
+    ofstream file(teachersFile, ios::app);
+    if (file.is_open()) {
+        file << t.getId() << "|"
+            << t.getName() << "|"
+            << t.getEmail() << "|"
+            << t.getAverageScore() << "\n";
+        file.close();
+    }
+}
+
+void DatabaseManager::saveStudent(string id, string name, string type, double gpa, string extra) 
+{
+    ofstream file(studentsFile, ios::app);
+    if (file.is_open())
+    {
+        file << id << "|" << name << "|" << type << "|" << gpa << "|" << extra << "\n";
+        file.close();
+    }
+}
+
+void DatabaseManager::loadTeachers(Teacher teachers[], int& count) 
+{
+    ifstream file(teachersFile);
+    count = 0;
+    if (!file.is_open())
+        return;
+    string line;
+    while (getline(file, line) && count < 50)
+    {
+        if (line.empty()) 
+            continue;
+        string id = getPart(line, 0);
+        string name = getPart(line, 1);
+        string email = getPart(line, 2);
+        teachers[count] = Teacher(id, name, email);
+        count++;
+    }
+    file.close();
+}
+void DatabaseManager::loadStudents(Student* students[], int& count)
+{
+    ifstream file(studentsFile);
+
+    count = 0;
+
+    if (!file.is_open())
+        return;
+
+    string line;
+
+    while (getline(file, line))
+    {
+        string id = getPart(line, 0);
+        string name = getPart(line, 1);
+        string type = getPart(line, 2);
+
+        if (type == "Regular")
+        {
+            students[count] =
+                new RegularStudent(id, name, "");
+        }
+
+        else if (type == "Scholarship")
+        {
+            students[count] =
+                new ScholarshipStudent(id, name, "", 2.5);
+        }
+
+        else if (type == "Exchange")
+        {
+            students[count] =
+                new ExchangeStudent(id, name, "");
+        }
+
+        count++;
+    }
+
+    file.close();
+}
+
+
