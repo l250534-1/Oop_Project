b01986c8260c10dc765fe92f02ee2321e0ca4dbb
l250534-1 <l250534@lhr.nu.edu.pk>
2026-05-09 19:57:36 +0500

    Added database manager(venue,section)

--- l250534__ooppp/DatabaseManager.cpp
+++ l250534__ooppp/DatabaseManager.cpp
@@ -116,4 +116,69 @@ void DatabaseManager::loadStudents(Student* students[], int& count)
     file.close();
 }
 
+void DatabaseManager::saveVenue(Venue& v) 
+{
+    ofstream file(venuesFile, ios::app);
+    if (file.is_open()) {
+        file << v.getRoomId() << "|"
+            << v.getCapacity() << "|"
+            << v.getHasComputers() << "\n";
+        file.close();
+    }
+}
 
+void DatabaseManager::saveSection(Section& s)
+{
+    ofstream file(sectionsFile, ios::app);
+    if (file.is_open())
+    {
+        file << s.getSectionId() << "|"
+            << s.getCourseId() << "|"
+            << s.getTeacherId() << "|"
+            << s.getVenueId() << "|"
+            << s.getTimeSlot() << "\n";
+        file.close();
+    }
+}
+
+void DatabaseManager::loadVenues(Venue venues[], int& count) 
+{
+    ifstream file(venuesFile);
+    count = 0;
+    if (!file.is_open()) 
+        return;
+    string line;
+    while (getline(file, line) && count < 20) 
+    {
+        if (line.empty()) 
+            continue;
+        string roomId = getPart(line, 0);
+        int    cap = stoi(getPart(line, 1));
+        bool   comp = stoi(getPart(line, 2));
+        venues[count] = Venue(roomId, cap, comp);
+        count++;
+    }
+    file.close();
+}
+
+void DatabaseManager::loadSections(Section sections[], int& count) 
+{
+    ifstream file(sectionsFile);
+    count = 0;
+    if (!file.is_open()) 
+        return;
+    string line;
+    while (getline(file, line) && count < 100) {
+        if (line.empty())
+            continue;
+        string sid = getPart(line, 0);
+        string cid = getPart(line, 1);
+        string tid = getPart(line, 2);
+        string vid = getPart(line, 3);
+        string slot = getPart(line, 4);
+        sections[count] = Section(sid, cid, tid);
+        sections[count].assignVenue(vid, slot);
+        count++;
+    }
+    file.close();
+}
--- l250534__ooppp/DatabaseManager.h
+++ l250534__ooppp/DatabaseManager.h
@@ -1,1 +1,31 @@
 #pragma once
+#include "Teacher.h"
+#include "RegularStudent.h"
+#include "ScholarshipStudent.h"
+#include "ExchangeStudent.h"
+#include "Venue.h"
+#include "Section.h"
+#include <fstream>
+#include<string>
+using namespace std;
+
+class DatabaseManager {
+private:
+    string studentsFile;
+    string teachersFile;
+    string venuesFile;
+    string sectionsFile;
+    string getPart(string line, int index);
+
+public:
+    DatabaseManager();
+    void saveTeacher(Teacher& t);
+    void saveStudent(string id, string name, string type, double gpa, string extra);
+    void saveVenue(Venue& v);
+    void saveSection(Section& s);
+    void loadStudents(Student* students[], int& count);
+    void loadTeachers(Teacher teachers[], int& count);
+    void loadVenues(Venue venues[], int& count);
+    void loadSections(Section sections[], int& count);
+};
+
