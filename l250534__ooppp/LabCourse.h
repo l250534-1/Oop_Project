15165ba078b188f9a9523d771e0229118fdaef0a
l250534-1 <l250534@lhr.nu.edu.pk>
2026-05-09 07:50:10 +0500

    added code for LabCourse

--- l250534__ooppp/LabCourse.h
+++ l250534__ooppp/LabCourse.h
@@ -1,1 +1,12 @@
 #pragma once
+#include "Course.h"
+
+class LabCourse : public Course {
+private:
+    bool requiresComputers;
+public:
+    LabCourse(string courseId, string title, string teacherId);
+    double calculateFinalGrade();
+    int    getExamDuration();
+    bool   getRequiresComputers();
+};
