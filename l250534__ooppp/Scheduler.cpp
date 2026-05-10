b1be6c4b41697e80c41b0e3bf6a2d8ffe33c62b1
l250534-1 <l250534@lhr.nu.edu.pk>
2026-05-10 06:49:52 +0500

    added dummy function for scheduler.cpp

---
+++ l250534__ooppp/Scheduler.cpp
@@ -0,0 +1,42 @@
+#include "Scheduler.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+Scheduler::Scheduler()
+{
+	//timeslots
+
+}
+void Scheduler::addSection(Section s)
+{
+
+}
+void Scheduler::addVenue(Venue v) 
+{
+
+}
+
+bool Scheduler::isVenueBooked(string venueId, string timeSlot)
+{
+
+}
+
+string Scheduler::suggestNextSlot(string venueId)
+{
+
+}
+
+void Scheduler::generateExamSchedule(Course* courses[], int courseCount)
+{
+
+}
+
+void Scheduler::saveSchedule()
+{
+
+}
+
+void Scheduler::displaySchedule() 
+{
+}
