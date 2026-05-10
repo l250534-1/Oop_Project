72d5bc4179d22b02fe058d398b4bff8ad4a72111
l250534-1 <l250534@lhr.nu.edu.pk>
2026-05-10 07:02:05 +0500

    timeslots

--- l250534__ooppp/Scheduler.cpp
+++ l250534__ooppp/Scheduler.cpp
@@ -6,6 +6,14 @@ using namespace std;
 Scheduler::Scheduler()
 {
 	//timeslots
+	sectionCount = 0;
+	venueCount = 0;
+	
+	timeSlots[0] = "Mon 08:00-11:00";
+	timeSlots[1] = "Mon 11:30-14:30";
+	
+	timeSlots[2] = "Mon 15:00-17:00";
+	timeSlots[3] = "Mon 17:00-18:00";
 
 }
 void Scheduler::addSection(Section s)
