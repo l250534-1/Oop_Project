47ba37f9e8283bbfff8072743db35af85cd6d4d0
l250534-1 <l250534@lhr.nu.edu.pk>
2026-05-10 07:34:24 +0500

    fixed lab course tested main for scheduler

--- l250534__ooppp/LabCourse.cpp
+++ l250534__ooppp/LabCourse.cpp
@@ -1,11 +1,19 @@
-#include "Course.h"
+#include "LabCourse.h"
+using namespace std;
 
-class LabCourse : public Course {
-private:
-    bool requiresComputers;
-public:
-    LabCourse(string courseId, string title, string teacherId);
-    double calculateFinalGrade();
-    int    getExamDuration();
-    bool   getRequiresComputers();
-};
+LabCourse::LabCourse(string courseId, string title, string teacherId)
+    : Course(courseId, title, teacherId, "Lab") {
+    requiresComputers = true;
+}
+
+double LabCourse::calculateFinalGrade() {
+    double total = 0;
+    for (int i = 0; i < assessmentCount; i++) {
+        if (assessments[i]->getType() != "Exam")
+            total += assessments[i]->calculateWeightedScore();
+    }
+    return total;
+}
+
+int  LabCourse::getExamDuration() { return 0; }
+bool LabCourse::getRequiresComputers() { return requiresComputers; }
