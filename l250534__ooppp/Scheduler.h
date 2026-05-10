a730d8c8fa45ae8b6b569b70a8ad31aa506b86b0
l250534-1 <l250534@lhr.nu.edu.pk>
2026-05-10 06:44:53 +0500

    aded odefr Scheduler.h

---
+++ l250534__ooppp/Scheduler.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Section.h"
+#include "Venue.h"
+#include "Course.h"
+#include "LabCourse.h"
+#include <fstream>
+using namespace std;
+class Scheduler {
+private:
+    Section sections[100];
+    int     sectionCount;
+    Venue   venues[20];
+    int     venueCount;
+    string  timeSlots[10];
+    int     timeSlotCount;
+
+public:
+    Scheduler();
+    void   addSection(Section s);
+    void   addVenue(Venue v);
+    bool   isVenueBooked(string venueId, string timeSlot);
+    string suggestNextSlot(string venueId);
+    void   generateExamSchedule(Course* courses[], int courseCount);
+    void   saveSchedule();
+    void   displaySchedule();
+};
