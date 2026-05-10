63a95110e769a80b8afd11518cdf54028d3f3339
l250534-1 <l250534@lhr.nu.edu.pk>
2026-05-10 07:15:26 +0500

    added timeslots

--- l250534__ooppp/Scheduler.cpp
+++ l250534__ooppp/Scheduler.cpp
@@ -14,6 +14,23 @@ Scheduler::Scheduler()
 	
 	timeSlots[2] = "Mon 15:00-17:00";
 	timeSlots[3] = "Mon 17:00-18:00";
+	timeSlots[4] = "Tue 8:00-10:00";
+	timeSlots[5] = "Tue 10:00-11:00";
+	timeSlots[6] = "Tue 11:00-14:00";
+
+	timeSlots[7] = "Tue 14:30-17:30";
+	timeSlots[8] = "Wed 8:00-11:00";
+	timeSlots[9] = "Wed 11:30-14:30";
+
+	timeSlots[10] = "Wed 15:00-17:00";
+	timeSlots[11] = "Wed 17:00-18:00";
+	timeSlots[12] = "Thurs 8:00-10:00";
+	timeSlots[13] = "Thurs 10:00-11:00";
+	timeSlots[14] = "Thurs 11:00-14:00";
+	timeSlots[15] = "Thurs 14:30-17:30";
+	timeSlots[16] = "Fri    8:00-11:00";
+	timeSlots[17] = "Fri    14:30-16:30";
+	timeSlotCount = 17;
 
 }
 void Scheduler::addSection(Section s)
